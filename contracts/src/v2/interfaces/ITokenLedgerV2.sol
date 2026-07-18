d6fb3d9059e041c8be9d811bceec4d2c2b40f41e
Tom <123529054+tomblanchard312@users.noreply.github.com>
2026-07-18 00:35:10 -0400

    Require scoped client keys for controller burns

--- contracts/src/v2/interfaces/ITokenLedgerV2.sol
+++ contracts/src/v2/interfaces/ITokenLedgerV2.sol
@@ -11,6 +11,5 @@ interface ITokenLedgerV2 {
 
     function controllerMove(address from, address to, uint256 amount) external;
     function controllerMint(address to, uint256 amount, bytes32 clientKey) external;
-    function controllerBurn(address from, uint256 amount) external;
     function controllerBurn(address from, uint256 amount, bytes32 clientKey) external;
 }
